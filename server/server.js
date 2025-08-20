132c77f57a7dbe307eb52f8845c7f5f637d7755c
vishalP143 <vishal.p8070@gmail.com>
2025-08-20 09:41:00 +0000

    basic

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const connectDB = require("./config");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
+connectDB();
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Chat app backend running");
+});
+
+// Socket test
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
